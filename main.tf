f597c7bf7ae9c7a4911106f32b2c71c1c3705bdf
benjamhooper <bhooper11231@gmail.com>
2023-04-24 22:28:11 -0500

    initial state of octopus deploy

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+    cloud {
+    organization = "benhooper"
+    workspaces {
+      name = "octopusdeployterraform"
+    }
+  }
   required_providers {
     octopusdeploy = {
       source = "OctopusDeployLabs/octopusdeploy"
@@ -16,4 +22,26 @@ provider "octopusdeploy" {
 resource "octopusdeploy_space" "Default" {
   name        = "Default"
   is_default  = true
+}
+
+resource "octopusdeploy_team" "oct-admin" {
+    id = "teams-administrators"
+    name = "Octopus Administrators"
+    users = [ "Users-1" ]
+}
+
+resource "octopusdeploy_team" "everyone" {
+    id = "teams-everyone"
+    name = "Everyone"
+    users = [ "Users-1" ]
+}
+
+resource "octopusdeploy_team" "teams-managers" {
+    id = "teams-managers"
+    name = "Octopus Managers"
+}
+
+resource "octopusdeploy_lifecycle" "Default-Lifecycle" {
+  name        = "Default Lifecycle"
+  id          = "Lifecycles-1"
 }
